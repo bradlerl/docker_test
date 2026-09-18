62adbcf86d2094ff80b3bd3366bd7f9c864ee0e3
Carla Bradley <bradlerl@purdue.edu>
2026-09-18 11:31:01 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const sql = require('mssql');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.static(__dirname));
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const config = {
+    user: 'Bradrachel',
+    password: 'Rach#0605',
+    database: 'virtual_escape_room',
+    options: { trustServerCertificate: true}
+};
+
+app.get('/api/data', async (req, res) => {
+    try {
+        console.log("Connecting to SQL Server...")
+        await sql.connect(config);
+        const result = await sql.query`SELECT * FROM Rivals`;
+        res.json(result.recordset);
+    } catch (err) {
+        console.error("SQL ERROR:", err);
+        res.status(500).send(err.message);
+    }
+});
+
+app.listen(3001, '10.193.91.232', () => console.log('Server running'));
